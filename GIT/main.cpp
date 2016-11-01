b513cd26ed7e87e0e5bfd8fefc71de8cfcca6345
CSCIMattAustin <matgaust@ut.utm.edu>
2016-11-01 10:57:29 -0500

    Adding sum function, user interaction

--- GIT/main.cpp
+++ GIT/main.cpp
@@ -2,10 +2,20 @@
 
 using namespace std;
 
+int sum(int n);
 int main(){
-
+  int n;
+  
   cout << "Hello World!!!!" << endl;
-
+  cout << "Please enter an integer: ";
+  cin >> n;
+  cout << "Your sum is: " << sum(n) << endl;
 
   return 0;
+}
+int sum(int n){
+  if (n == 0)
+    return 0;
+  else
+    return n + sum(n-1);
 }
