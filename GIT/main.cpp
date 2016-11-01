a1af6ae4e9b4779dafaa181ab9a68af08d319412
CSCIMattAustin <matgaust@ut.utm.edu>
2016-11-01 11:29:54 -0500

    added product function

--- GIT/main.cpp
+++ GIT/main.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 int sum(int n);
+long int product(int n);
 int main(){
   int n;
   
   cout << "Hello World!!!!" << endl;
   cout << "Please enter an integer: ";
   cin >> n;
-  cout << "Your sum is: " << sum(n) << endl;
-
+  cout << "Your sum from 1 to " << n << " is: " << sum(n) << endl;
+  cout << "Your product from 1 to " << n << " is: " << product(n) << endl;
   return 0;
 }
 int sum(int n){
@@ -18,4 +19,10 @@ int sum(int n){
     return 0;
   else
     return n + sum(n-1);
+}
+long int product(int n){
+  if (n < 1)
+    return 1;
+
+    return n * product(n-1);
 }
